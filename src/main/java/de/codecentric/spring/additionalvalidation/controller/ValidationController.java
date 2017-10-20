ddcb5fcae91c19dbbe06c2b733abf4367c5d590d
Ronny Bräunlich <ronny.braeunlich@codecentric.de>
2017-10-20 15:13:40 +0200

    change annotation to @Valid

--- src/main/java/de/codecentric/spring/additionalvalidation/controller/ValidationController.java
+++ src/main/java/de/codecentric/spring/additionalvalidation/controller/ValidationController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 @RestController
 public class ValidationController {
@@ -28,7 +29,7 @@ public class ValidationController {
     }
 
     @RequestMapping(value = "/validation", method = RequestMethod.POST)
-    public ResponseEntity<?> acceptData(@Validated @RequestBody Data data, Errors errors, @RequestHeader(HttpHeaders.ACCEPT_LANGUAGE) String language) {
+    public ResponseEntity<?> acceptData(@Valid @RequestBody Data data, Errors errors, @RequestHeader(HttpHeaders.ACCEPT_LANGUAGE) String language) {
         stringValueValidator.validate(language, data, errors);
         if (errors.hasErrors()) {
             return new ResponseEntity<>(createErrorString(errors), HttpStatus.BAD_REQUEST);
